17aba73460b4c80eaa418fb3eb3c2f98a7016948
corri-filippo <corrifilippo@me.com>
2023-01-05 09:10:59 +0100

    main.c created

--- cursus/core/main.c
+++ cursus/core/main.c
@@ -1,27 +1,10 @@
+#include <stdarg.h>
 #include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include "libft/libft.h"
 
 int main(void)
 {
-	int	reading = open("lorem", O_RDONLY);
-	char	**output;
-	char	*buffer;
-	size_t	i = 0;
-	char	*line;
+	int	num = 0;
 
-	buffer = (char *) malloc(sizeof(char) * 446);
-	read(reading, buffer, 447);
-	output = ft_split("          ", ' ');
-	while ((line = output[i++]))
-	{
-		write(1, line, ft_strlen(line));
-		write(1, "\n", 1);
-		free(line);
-	}
-	close(reading);
-	free(output);
+	printf("%10\n", num);
 	return (0);
 }
